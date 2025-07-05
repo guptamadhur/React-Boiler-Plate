1f40402e9d760b996fc410ca01892a7fc87e9a92
Madhur Gupta <guptamadhur96@gmail.com>
2025-07-06 02:07:19 +0530

    port revert

--- server/index.ts
+++ server/index.ts
@@ -50,7 +50,7 @@ async function startServer() {
     });
   }
 
-  const port = process.env.PORT || 8080;
+  const port = process.env.PORT || 5173;
   app.listen(port, () => {
     console.log(`🚀 Server running at http://localhost:${port}`);
   });
